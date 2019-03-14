00341ef2b683494472dcd8302696640f6e0e5e7b
Yuoa <jio@yuoa.dev>
2019-03-14 06:49:06 +0000

    same application in setup_monitor_csi.sh

--- injection/setup_monitor_csi.sh
+++ injection/setup_monitor_csi.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/sudo /bin/bash
-modprobe -r iwlwifi mac80211 cfg80211
 modprobe iwlwifi connector_log=0x1
 # Setup monitor mode, loop until it works
-iwconfig wlan0 mode monitor 2>/dev/null 1>/dev/null
+iwconfig wlp1s0 mode monitor 2>/dev/null 1>/dev/null
 while [ $? -ne 0 ]
 do
-	iwconfig wlan0 mode monitor 2>/dev/null 1>/dev/null
+	iwconfig wlp1s0 mode monitor 2>/dev/null 1>/dev/null
 done
-iw wlan0 set channel $1 $2
-ifconfig wlan0 up
+iw wlp1s0 set channel $1 $2
+ifconfig wlp1s0 up
