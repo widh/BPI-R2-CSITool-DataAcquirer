100ff3449ae0d4ff2fe4381af085fb019dae8430
Jio <jio@widh.me>
2019-03-28 19:56:32 +0800

    Stdout messaging of capturer optimized

--- capture/log_to_file.c
+++ capture/log_to_file.c
@@ -82,20 +82,13 @@ int main(int argc, char** argv)
 			exit_program_err(-1, "recv");
 		/* Pull out the message portion and print some stats */
 		cmsg = NLMSG_DATA(buf);
-		printf(".");
-		// printf("[bytelen=%d] ", cmsg->len);
-		// if (count % SLOW_MSG_CNT == 0)
-		// 	printf("received %d bytes: id: %d val: %d seq: %d clen: %d\n", cmsg->len, cmsg->id.idx, cmsg->id.val, cmsg->seq, cmsg->len);
-		// else
-		// 	printf("\n");
 		/* Log the data to file */
 		l = (unsigned short) cmsg->len;
 		l2 = htons(l);
 		fwrite(&l2, 1, sizeof(unsigned short), out);
 		ret = fwrite(cmsg->data, 1, l, out);
-		if (count % 100 == 0)
-			printf("* = wrote %d bytes [msgcnt=%u]\n", ret, count);
-		++count;
+		if (++count % 100 == 0)
+			printf("Wrote %d bytes [msgcnt=%u]\n", ret, count);
 		if (ret != l)
 			exit_program_err(1, "fwrite");
 	}
